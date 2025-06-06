66f851fab345206df6659a664026279f7e689bf6
AmrutaJayanti <142327526+AmrutaJayanti@users.noreply.github.com>
2025-06-06 10:22:37 +0530

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -49,7 +49,8 @@ app.use(
 	cors({
 		origin: [
 			'http://localhost:5173',
-			'https://livenetworkgames.netlify.app'
+			'https://livenetworkgames.netlify.app',
+			'https://livenetworkgames.netlify.app/'
 		],
 	}),
 );
@@ -73,7 +74,8 @@ const io: Server = new Server(server, {
 	cors: {
 		origin: [
 			'http://localhost:5173',
-			'https://livenetworkgames.netlify.app'
+			'https://livenetworkgames.netlify.app',
+			'https://livenetworkgames.netlify.app/'
 		],
 	},
 });
